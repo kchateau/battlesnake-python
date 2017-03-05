d576405e57aed3da0f051f0e89087f6bb4c6dc61
Kendra Chateau <Kendra@Kendras-MacBook-Air.local>
2017-03-04 16:46:19 -0800

    copy of battle snake

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#FF0000',
+        'color': '#00FF00',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
         'name': 'Kendra Awesomer Snake'
